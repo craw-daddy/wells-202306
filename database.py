d765797f4b606f693714b5c3e842e6768fe495d7
Russell Martin <russell@martinandbrown.com>
2023-07-13 20:27:43 +0000

    Add database.py file and initial requirements.txt file

---
+++ database.py
@@ -0,0 +1,33 @@
+from sqlalchemy import create_engine, text
+import os
+
+WELLS_URL = os.getenv('WELLS_URL')
+
+def query_db(min_depth, min_gradient):
+    """Returns wells that fit the search criteria."""
+
+    engine = create_engine(WELLS_URL)
+    
+    query = text(
+            """SELECT latitude, longitude, gradient
+            FROM wells
+            WHERE depth > :min_depth AND gradient > :min_gradient
+            """
+            )
+    with engine.connect() as conn:
+        results = (
+                conn
+                .execute(query, {'min_depth': min_depth, 'min_gradient': min_gradient})
+                .fetchall()
+                )
+
+    return results
+
+if __name__ == '__main__':
+    import sys
+
+    min_depth = float(sys.argv[1])
+    min_gradient = float(sys.argv[2])
+
+    print(query_db(min_depth, min_gradient))
+
