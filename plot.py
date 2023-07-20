0f554fad7e61f92f1e375322d7bcb2435db8eb7c
Russell Martin <russell@martinandbrown.com>
2023-07-20 15:34:06 +0000

    Add map building notebook and plot.py script

---
+++ plot.py
@@ -0,0 +1,29 @@
+from vega_datasets import data
+import altair as alt
+import pandas as pd
+
+def plot_wells(well_coords):
+    #  Create the US map
+    states = alt.topo_feature(data.us_10m.url, feature='states')
+    USmap = (alt.Chart(states)
+             .mark_geoshape(fill='lightgray', stroke='white')
+             .project('albersUsa')
+             .properties(width=600, height=400)
+            )
+
+    #  Add the wells information to map
+    columns = ['latitude', 'longitude', 'depth', 'gradient']
+    well_coords = pd.DataFrame(well_coords, columns=columns)
+    well_coordinates = (alt.Chart(well_coords)
+                        .mark_circle()
+                        .encode(latitude='latitude',
+                                longitude='longitude',
+                                color=alt.Color('gradient', title='Gradient',
+                                               scale=alt.Scale(scheme='inferno')),
+                                tooltip=[alt.Tooltip('depth', title='Depth (m)'),
+                                         alt.Tooltip('gradient', title='Gradient (°C/m)',
+                                                    format='0.2f')])
+                       )
+
+    return USmap + well_coordinates
+
